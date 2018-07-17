21cd99518a9d2d8b1eb81cd84f173cf37dd62781
Alex Brooke <akb.sbc@gmail.com>
2018-07-17 16:34:38 -0500

    use pyenv instead of python from homebrew
    testing to see if some issues are resolved after downgrading from 3.7

--- scripts/install_mac_build_dependencies.sh
+++ scripts/install_mac_build_dependencies.sh
@@ -2,13 +2,15 @@
 
 echo =============== INSTALLING PYTHON3 ===============
 
-brew upgrade python
+brew upgrade pyenv
 
 if [ $? -ne 0 ]
 then
-  echo Failed to install python
+  echo Failed to install pyenv
   exit 1
 fi
+
+pyenv install 3.6.5
 
 echo =============== INSTALLING PIP DEPENDENCIES ===============
 
